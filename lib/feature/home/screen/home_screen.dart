aa058b14579eb6de17816b76165593758e548461
Yu Honda <yh.lzusnclfb@gmail.com>
2026-07-03 09:21:45 +1000

    Create initial HomeScreen

---
+++ lib/feature/home/screen/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+    const HomeScreen({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: const Text('Household Budget'),
+                ),
+            body: const Center(
+                child: Text('Welcome to Household Budget'),
+            )
+        );
+    }
+
+}
